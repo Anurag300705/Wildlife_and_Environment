8e7f36ef95be0365b9aa15eda19dbcf7e1763b3f
jyotiska222 <j.biswas0022@gmail.com>
2024-08-07 21:30:39 +0530

    Navbar and Section01

---
+++ my-app/src/components/Section01.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Section01.css';
+
+const Section01 = () => {
+  return (
+    <div className="section-container">
+      <img src=".\src\assets\image.jpg" alt="Sanctuary Map" className="section-image" />
+      <div className="section-text">
+        <p>The Ministry of Tourism (MOT) of India oversees the promotion and development of wildlife sanctuaries. Its website provides comprehensive information on various sanctuaries across the country, detailing their unique flora and fauna, visitor guidelines, and conservation efforts. This resource aids tourists in planning eco-friendly and educational trips.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Section01;
