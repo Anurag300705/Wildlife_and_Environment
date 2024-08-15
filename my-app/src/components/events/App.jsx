a75ab731478f22df9b696cf85f244c4aff13baee
Anurag <anuragchaudhuri3007@gmail.com>
2024-08-15 21:42:07 +0530

    added events section

---
+++ my-app/src/components/events/App.jsx
@@ -0,0 +1,13 @@
+import Events from './events.jsx'
+import Container from './container.jsx'
+
+function Section2() {
+
+  return (
+    <>
+      <Container/>
+    </>
+  )
+}
+
+export default Section2;
---
+++ my-app/src/components/events/container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './events.jsx'
+import Events from './events.jsx'
+
+function container() {
+  return (
+    <div className="Container">
+        <Events></Events>
+        <Events></Events>
+        <Events></Events>
+    </div>
+  )
+}
+
+export default container
